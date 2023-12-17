acf42d149402228939068357e353dd19584cb2a7
s2drag0n <2558058352@qq.com>
2023-12-17 22:56:40 +0800

    little change

--- lua/plugins/color-theme.lua
+++ lua/plugins/color-theme.lua
@@ -1,14 +1,5 @@
 return {
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
-      style = "storm",
-      transparent = true,
-      styles = {
-        sidebars = "transparent", -- style for sidebars, see below
-        floats = "transparent", -- style for floating windows
-      },
-    },
+    "xiyaowong/transparent.nvim",
   },
 }
