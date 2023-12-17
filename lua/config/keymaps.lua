0ee8aa7d81cfffd347b3d453cbc32e0bfabbcc11
s2drag0n <2558058352@qq.com>
2023-12-17 22:22:35 +0800

    totally change

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+-- 之后就可以这样映射按键了 -- map('模式','按键','映射为XX',opt)
+map("i", "jj", "<ESC>", opts)
---
+++ lua/plugins/color-theme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+      style = "storm",
+      transparent = true,
+      styles = {
+        sidebars = "transparent", -- style for sidebars, see below
+        floats = "transparent", -- style for floating windows
+      },
+    },
+  },
+}
